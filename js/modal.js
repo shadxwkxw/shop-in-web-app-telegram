3c83cc5c6ffbf94572205e257d5870087204fce1
shadxwkxw <lolkek2468@gmail.com>
2024-09-26 23:54:12 +0300

    adding a modal window and content

--- js/modal.js
+++ js/modal.js
@@ -7,18 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.querySelector('.modal-image').src = imageSrc;
         modal.querySelector('.modal-title').textContent = title;
         modal.querySelector('.modal-description').textContent = description;
-        modal.style.display = 'block';
+
+        modal.style.display = 'flex';
+        setTimeout(() => {
+            modal.classList.add('show');
+        }, 10);
     }
 
     // Обработчик события для закрытия модального окна
     closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
+        modal.classList.remove('show'); 
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
     });
 
     // Закрытие модального окна при клике вне его
     window.addEventListener('click', (event) => {
         if (event.target === modal) {
-            modal.style.display = 'none';
+            modal.classList.remove('show');
+            setTimeout(() => {
+                modal.style.display = 'none';
+            }, 300);
         }
     });
 
@@ -27,8 +37,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     productImages.forEach((image, index) => {
         image.addEventListener('click', () => {
-            const title = `Товар ${index + 1}`;
-            const description = `Описание товара ${index + 1}`; // Описание
+            const title = image.getAttribute('data-title');
+            const description = image.getAttribute('data-description'); // Описание
             const imageSrc = image.src;
 
             openModal(imageSrc, title, description);
